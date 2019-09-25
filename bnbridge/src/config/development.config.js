0e6459adf17294e9a0e05bf08cee56e8de3e069a
Minh Doan <minh@harmony.one>
2019-09-25 02:08:05 +0000

    fix db access on ec2

--- bnbridge/src/config/development.config.js
+++ bnbridge/src/config/development.config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: "http://localhost:5000",
+  apiUrl: "https://bnbridge.harmony.one",
   apiToken: "ZTgwMTY1NjkzZjAyOTk1N2VjNDQ4MjBhNGRiODJiMGI1NjI5YjM2YjJkNjc1YjVhYjE0YmEwNTBhMDFiNDk3ZDpmYmM3MWMyOTRmOWE4N2VlM2QzMmVkZDVkNjExNTE4MTFlNDRmNzc0NDgzNzY4OWVmYWRkYmJiOWY3NjgxYzA5",
   explorerURL: "https://testnet-explorer.binance.org/tx/",
   bnbAddressLength: 43,
